cc819422089410a306980b9a417593518966dc1c
Eric Feldman <ericfeldman2@gmail.com>
2020-04-17 08:04:16 -0400

    working on integrating bootstrap for navbar

--- src/Nav.js
+++ src/Nav.js
@@ -7,7 +7,30 @@ export default class Nav extends Component {
   render() {
     return (
       <Content className='Nav'>
-        <div className="name">
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container"> 
+         <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-nav-demo" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+        <a href="#" class="navbar-brand"><span class="glyphicon glyphicon-picture" aria-hidden="true"></span> IMGS</a>
+    </div>
+    <div class="collapse navbar-collapse" id="bs-nav-demo">
+    <ul class="nav navbar-nav">
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Sign Up</a></li>
+        <li><a href="#">Log In</a></li>
+    </ul>
+    </div>
+</div>  
+</nav>
+        {/* <div className="name">
         <p>Hannah T Skokan</p>
         </div>
         <div className="bar">
@@ -25,8 +48,8 @@ export default class Nav extends Component {
         </Link>
         <Link className='Link' to='/contact'>
           Contact
-        </Link>
-        </div>
+        </Link> */}
+        {/* </div> */}
       </Content>
     )
   }
