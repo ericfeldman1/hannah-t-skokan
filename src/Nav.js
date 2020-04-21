c0414be35d497aecc5a97df9eff8984a80da8a91
Eric Feldman <ericfeldman2@gmail.com>
2020-04-20 21:55:14 -0400

    paritally working navbar w bootstrap

--- src/Nav.js
+++ src/Nav.js
@@ -7,28 +7,19 @@ export default class Nav extends Component {
   render() {
     return (
       <Content className='Nav'>
-        <nav class="navbar navbar-inverse navbar-fixed-top">
-        <div class="container"> 
-         <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-nav-demo" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-        <a href="#" class="navbar-brand"><span class="glyphicon glyphicon-picture" aria-hidden="true"></span> IMGS</a>
+<nav className="navbar navbar-expand-lg">
+  <Link className="navbar-brand" to="/">Hannah T Skokan</Link>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div className="navbar-nav">
+      {/* <Link className="Link nav-item nav-link active" to="/">Home <span className="sr-only">(current)</span></Link> */}
+      <Link className="Link nav-item nav-link" to="/photos">Photos</Link>
+      <Link className="Link nav-item nav-link" to="/videos">Videos</Link>
+      <Link className="Link nav-item nav-link" to="/resume">Resume</Link>
     </div>
-    <div class="collapse navbar-collapse" id="bs-nav-demo">
-    <ul class="nav navbar-nav">
-        <li><a href="#">About</a></li>
-        <li><a href="#">Contact</a></li>
-    </ul>
-    <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Sign Up</a></li>
-        <li><a href="#">Log In</a></li>
-    </ul>
-    </div>
-</div>  
+  </div>
 </nav>
         {/* <div className="name">
         <p>Hannah T Skokan</p>
